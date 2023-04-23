42fd8a9c7c73b83f1e9c774c425b2a2833a0b72a
Etx17 <etiennededi@hotmail.fr>
2023-04-23 08:53:33 +0200

    Project initialisation with expo, setup fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/themes/colors';
+import  font from './src/themes/fonts';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{color: colors.primary, fontSize: font.size.md}}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/themes/fonts.ts
@@ -0,0 +1,29 @@
+const size = {
+    xs: 10,
+    sm: 12,
+    default: 14,
+    md: 16,
+    lg: 20,
+    xlg: 24,
+    xxlg: 30,
+}
+
+const weight = {
+    full: '900',
+    semi: '600',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400',
+    light: '300',
+    lighter: '200',
+    lightest: '100',
+
+
+}
+const type = {
+    base: 'Avenir-Book',
+    bold: 'Avenir-Black',
+    emphasis: 'HelveticaNeue-Italic',
+    light: 'Avenir-Light',
+}
+export default { size, weight };
