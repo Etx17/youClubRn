be17fe9cb55e6ceb4e6aac454c9ddf072463f15c
Etx17 <etiennededi@hotmail.fr>
2023-04-24 15:16:46 +0200

    work on array

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -14,30 +14,37 @@ interface IDropdownProps {
 
 const Dropdown = ({style, disabled, onValueChange, valuecat}: IDropdownProps) => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState("164_014");
   const [items, setItems] = useState([
-    {label: 'Apple big Apple i can tellyou a story about trhee little birds just by my doorstep, singin sweet song', value: 'apple',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Banae(-(yena', value: 'banazena',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Orange', value: 'orange',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Peach', value: 'peach',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Pineapple', value: 'pineapple',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Strawberry', value: 'strawberry',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Watermelon', value: 'watermelon',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Mango', value: 'mango',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Grape', value: 'grape',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Papaya', value: 'papaya',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Cherry', value: 'cherry',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Lemon', value: 'lemon',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Pomegranate', value: 'pomegranate',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Coconut', value: 'coconut',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Avocado', value: 'avocado',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Cucumber', value: 'cucumber',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Tomato', value: 'tomato',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Potato', value: 'potato',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Onion', value: 'onion',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
-    {label: 'Cabbage', value: 'cabbage',  icon: () =>  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />},
+    { label: "culture, pratiques d’activités artistiques, culturelles", value: "1", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7 }} /> },
+    { label: "Sports, activités de plein air", value: "2", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "amicales, groupements affinitaires, groupements d'entraide (hors défense de droits fondamentaux", value: "3", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "éducation formation", value: "4", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "clubs de loisirs, relations", value: "5", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "Environnement, cadre de vie", value: "6", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "conduite d’activités économiques", value: "7", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "action socio-culturelle", value: "8", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "associations caritatives, humanitaires, aide au développement, développement du bénévolat", value: "9", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "santé", value: "100", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "représentation, promotion et défense d’intérêts économiques", value: "11", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "interventions sociales", value: "12", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "information communication", value: "13", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "aide à l'emploi, développement local, promotion de solidarités économiques, vie locale", value: "14", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "défense de droits fondamentaux, activités civiques", value: "15", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "préservation du patrimoine", value: "16", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "activités religieuses, spirituelles ou philosophiques", value: "17", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "services familiaux, services aux personnes âgées", value: "18", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "activités politiques", value: "19", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "clubs, cercles de réflexion", value: "20", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "chasse pêche", value: "21", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "services et établissements médico-sociaux", value: "22", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "logement", value: "23", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "recherche", value: "24", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "armée (dont préparation militaire, médailles)", value: "25", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "Tourisme", value: "26", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "sécurité, protection civile", value: "27", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
+    { label: "justice", value: "28", icon: () => <AntIcons name="arrowright" size={30} color={colors.primaryLighter} /> },
 
-    // {label: 'Banana', value: 'banana',  }
   ]);
 
   return (
