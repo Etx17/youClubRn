3c494005338c9c38f861a024e4d0a74cc8b741d4
Etx17 <etiennededi@hotmail.fr>
2023-04-28 11:48:48 +0200

    got api result

--- src/screens/ClubsIndexScreen/ClubsIndexScreen.tsx
+++ src/screens/ClubsIndexScreen/ClubsIndexScreen.tsx
@@ -11,15 +11,38 @@ import { useLocationContext } from '../../contexts/LocationContext';
 const ClubsIndexScreen = () => {
   const [clubsList, setClubsList] = useState(clubs);
   const {lat, lon} = useLocationContext()
-    console.log(lat, lon, 'Current coordinates');
-
   const [dropdownValue, setDropdownValue] = useState("sports");
   const [subCategoryDropdownValue, setSubCategoryDropdownValue] = useState("all");
+
+  const fetchData = async () => {
+    const url = 'https://journal-officiel-datadila.opendatasoft.com/api/records/1.0/search/?dataset=jo_associations&q=NOT+objet%3A%22%22&lang=FR&rows=12&sort=dateparution&facet=source&facet=annonce_type_facette&facet=localisation_facette&facet=metadonnees_type_code&facet=lieu_declaration_facette&facet=domaine_activite_categorise&facet=domaine_activite_libelle_categorise&refine.domaine_activite_libelle_categorise=Sports%2C+activit%C3%A9s+de+plein+air&refine.typeavis=Cr%C3%A9ation&refine.commune_actuelle=Paris';
+
+    try {
+      const response = await fetch(url);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      const data = await response.json();
+      console.log(data.facet_groups[0].facets[0].count, 'There are 5314 associations in the sports category');
+      // console.log(data.records.length, 'this is data');
+      console.log(data.records[0].fields.domaine_activite_libelle_categorise.split('/'), 'this is first record data');
+      
+      // setClubsList(data.records);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // You can also set an error state here, if you want to display an error message in the UI.
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleDropdownValueChange = (valuecat: any) => {
     setDropdownValue(valuecat);
   };
-
   const handleSubCategoryDropdownValueChange = (valuesub: any) => {
     setSubCategoryDropdownValue(valuesub);
     console.log(valuesub, 'this is valuesub')
@@ -50,7 +73,7 @@ return (
    
       <Swiper
         cards={clubsList}
-        infinite={false}
+        infinite={true}
         stackSize={5}
         animateOverlayLabelsOpacity
         animateCardOpacity
