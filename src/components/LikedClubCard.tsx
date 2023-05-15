8858d1fbf9d40aafa2938c39a7061f6ff440f405
Etx17 <etiennededi@hotmail.fr>
2023-05-15 15:13:15 +0200

    wip

--- src/components/LikedClubCard.tsx
+++ src/components/LikedClubCard.tsx
@@ -1,24 +1,52 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Pressable, Alert } from 'react-native';
 import categoryImages from '../assets/data/categoryImages';
 import {Image} from 'expo-image';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntIcons from '@expo/vector-icons/AntDesign';
+import { useNavigation } from '@react-navigation/native';
+import colors from '../themes/colors';
 const LikedClubCard = ({ club, onUnLike }: any) => {
     const domaine_activite_libelle_categorise = club?.fields?.domaine_activite_libelle_categorise;
     const subCategory = domaine_activite_libelle_categorise ? domaine_activite_libelle_categorise.split('/')[1].split('###')[0].charAt(0).toUpperCase() + domaine_activite_libelle_categorise.split('/')[1].split('###')[0].slice(1) : 'Autre/Non renseigné';
     const category = domaine_activite_libelle_categorise ? domaine_activite_libelle_categorise.split('/')[0].split('###')[0].charAt(0).toUpperCase() + domaine_activite_libelle_categorise.split('/')[0].split('###')[0].slice(1) : 'Autre/Non renseigné';
-    console.log(categoryImages[category][subCategory], ' <= LIke : this is the image supposed keyword')
+    // console.log(categoryImages[category][subCategory], ' <= LIke : this is the image supposed keyword')
     const image = 
       `https://source.unsplash.com/random/?${categoryImages[category] ? categoryImages[category][subCategory] : 'random'}`
     ;
-  console.log(image)
+    const navigation = useNavigation();
   return (
     <View style={styles.container}>
         <Image source={{uri: image}} style={styles.image}/>
-        <View style={styles.textContainer}>
-            <Text style={styles.title}>{club?.fields?.titre}</Text>
-            <Text style={styles.category}>{category}</Text>
-            <Text style={styles.subcategory}>{subCategory}</Text>
-        </View>
+        {/* <View style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}> */}
+          <View style={styles.textContainer}>
+              <Text style={styles.title}>{club?.fields?.titre}</Text>
+              <Text style={styles.category}>{category}</Text>
+              <Text style={styles.subcategory}>{subCategory}</Text>
+              
+              <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                <Pressable onPress={() =>  Alert.alert('Bientôt disponible') }>
+                  {/* //  navigation.navigate('ClubDetails', {clubData: data.fields, images}) */}
+                
+                  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />
+                </Pressable>
+
+                <Pressable onPress={() =>  Alert.alert('Bientôt disponible') }>
+                  {/* //  navigation.navigate('ClubDetails', {clubData: data.fields, images}) */}
+                
+                  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />
+                </Pressable>
+
+
+                <Pressable onPress={() =>  Alert.alert('Bientôt disponible') }>
+                  {/* //  navigation.navigate('ClubDetails', {clubData: data.fields, images}) */}
+                
+                  <AntIcons name="arrowright" size={40} color={colors.primaryLighter} style={{ textAlign: 'center', textShadowColor: 'rgba(0, 0, 0, 0.30)', textShadowOffset: {width: 1, height: 1}, textShadowRadius: 7  }} />
+                </Pressable>
+              </View>
+          </View>
+         
+        {/* </View> */}
         {/* <Button title="Unlike" onPress={() => onUnLike(club.id)} /> */}
    </View>
   )
@@ -41,6 +69,7 @@ const styles = StyleSheet.create({
     textContainer: {
       flex: 1,
       marginLeft: 10,
+      justifyContent: 'space-around',
     },
     title: {
       fontSize: 18,
