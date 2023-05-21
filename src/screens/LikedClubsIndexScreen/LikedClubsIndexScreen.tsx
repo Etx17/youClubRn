6c9af9a8da01b3473c866e6e3bcb4c5cccefe347
Etx17 <etiennededi@hotmail.fr>
2023-05-21 22:33:44 +0200

    ok

--- src/screens/LikedClubsIndexScreen/LikedClubsIndexScreen.tsx
+++ src/screens/LikedClubsIndexScreen/LikedClubsIndexScreen.tsx
@@ -1,12 +1,7 @@
-import { View, Text, Alert, Pressable, StyleSheet, Dimensions } from 'react-native'
-import React, {useEffect, useState} from 'react'
+import { View, Text,StyleSheet, Dimensions } from 'react-native'
+import React, { useState} from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import Swiper from 'react-native-deck-swiper';
-import ClubCard from '../../components/ClubCard';
-import AntIcons from '@expo/vector-icons/AntDesign';
 import { useFocusEffect } from '@react-navigation/native';
-import colors from '../../themes/colors';
 import { FlashList } from "@shopify/flash-list";
 import LikedClubCard from '../../components/LikedClubCard';
 
@@ -51,7 +46,6 @@ useFocusEffect(
             data={likedClubs}
             keyExtractor={(item: any) => item?.fields?.id.toString()}
             renderItem={({ item }) => <LikedClubCard club={item} onUnLike={handleUnLike} />}
-            // ListHeaderComponent={ListHeaderComponent}
             estimatedItemSize={20}
           />
         </View>
@@ -68,7 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'transparent',
-    // backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   }
