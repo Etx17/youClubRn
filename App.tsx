4efe283d8a851932d19310af6121a6fc256104ab
Etx17 <etiennededi@hotmail.fr>
2023-06-24 11:03:09 +0200

    react native paper install

--- App.tsx
+++ App.tsx
@@ -4,18 +4,30 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import React from 'react'
 import Navigation from './src/navigation';
 import LocationContextProvider from './src/contexts/LocationContext';
+import {MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
 import AuthContextProvider from './src/contexts/AuthContext';
+import colors from './src/themes/colors';
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: colors.primary,
+      secondary: colors.secondary,
+    },
+  };
 
    return (
   // Ne wrapper le location context provider que autour du topTabNavigator là ou j'en ai besoin
-       <SafeAreaProvider>
-        <AuthContextProvider>
-          <LocationContextProvider>
-            <Navigation/>
-          </LocationContextProvider>
-        </AuthContextProvider>
-      </SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <AuthContextProvider>
+            <LocationContextProvider>
+              <Navigation/>
+            </LocationContextProvider>
+          </AuthContextProvider>
+        </SafeAreaProvider>
+      </PaperProvider>
   );
 }
 
--- src/screens/EditClubScreen/EditClubScreen.tsx
+++ src/screens/EditClubScreen/EditClubScreen.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import React from 'react'
-
+import { Button } from 'react-native-paper'
 const EditClubScreen = () => {
   return (
     <View>
       <Text>EditClubScreen</Text>
+      <Button onPress={()=> Alert.alert('Envoyer a l API')}mode='elevated' textColor='black'>Enregistrer</Button>
     </View>
   )
 }
