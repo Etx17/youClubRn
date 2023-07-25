9e272ccce5284dccd9459810ff2021ed4ec58cff
Etienne de Dianous <etiennededi@hotmail.fr>
2023-07-25 10:45:28 -0400

    created photosSection components

---
+++ src/components/photosSection.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { View, Text, FlatList, Image, Pressable, StyleSheet } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Button } from 'react-native-paper';
+import colors from '../themes/colors';
+
+type IPhotosSectionProps = {
+  existingImages: string[];
+  selectedImages: string[];
+  numRows: number;
+  pickImageAsync: () => void;
+  handleImageDelete: (image: string) => void;
+};
+
+const PhotosSection = ({ existingImages, selectedImages, numRows, pickImageAsync, handleImageDelete }: IPhotosSectionProps) => {
+  return (
+    <View>
+      {/* Component d'input pour des photos */}
+      {selectedImages.length > 0 || existingImages && existingImages.length > 0 ? (
+        <View style={{ borderWidth: 0, margin: 10 }}>
+          <FlatList
+            data={[...existingImages, ...selectedImages]}
+            keyExtractor={(item) => item}
+            numColumns={3}
+            scrollEnabled={false}
+            style={{ maxHeight: numRows * 150 }}
+            renderItem={({ item }) => (
+              <View style={styles.thumbnailContainer}>
+                <Image source={{ uri: item }} style={styles.thumbnail} />
+                <Pressable style={styles.deleteButton} onPress={() => handleImageDelete(item)}>
+                  <Ionicons name='trash-outline' size={20} color={colors.danger} />
+                </Pressable>
+              </View>
+            )}
+          />
+          <Text onPress={pickImageAsync} style={{ fontSize: 14, color: colors.grayDarkest, margin: 10 }}>
+            {selectedImages.length + existingImages.length} images sélectionnées <Text style={{ color: colors.danger }}>(Ajouter)</Text>
+          </Text>
+        </View>
+      ) : (
+        <Button onPress={pickImageAsync} style={styles.imageButton}>
+          <Text style={styles.addImageText}>+</Text>
+        </Button>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  deleteButton: {
+    position: 'absolute',
+    bottom: -10,
+    right: 0,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    shadowOffset: { width: 2, height: 0 }, // iOS only
+    shadowOpacity: 0.8, // iOS only
+    elevation: 8, // Android only
+  },
+  thumbnailContainer: {
+    margin: 4,
+    marginBottom: 10,
+    width: "30%",
+    aspectRatio: 1,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black'
+    },
+  thumbnail: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10
+  },
+  imageButton: {
+    width: "95%",
+    height: 100,
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#ccc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addImageText: {
+    fontSize: 60,
+    color: "lightgrey",
+  },
+})
+export default PhotosSection;
--- src/screens/NewActivityScreen/NewActivityScreen.tsx
+++ src/screens/NewActivityScreen/NewActivityScreen.tsx
@@ -10,7 +10,7 @@ import { useNavigation } from '@react-navigation/native';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ActivitySchema, Activity } from '../../schema/activity.schema';
 import ControlledInput from '../../components/ControlledInput';
-import { useAuthContext } from '../../contexts/AuthContext';
+import PhotosSection from '../../components/photosSection';
 import Dropdown from '../../components/Dropdown';
 import SubCategoryDropdown from '../../components/SubCategoryDropdown';
 import { Storage } from 'aws-amplify';
@@ -25,10 +25,11 @@ const NewActivityScreen = (clubId: string) => {
   const [isImagePickerVisible, setImagePickerVisible] = useState(false);
   const [subCategoryDropdownValue, setSubCategoryDropdownValue] = useState("all");
   const [isSubmitting, setIsSubmitting] = useState(false)
-  const navigation = useNavigation()
   const [selectedImages, setSelectedImages] = useState<string[]>([]);
-  const numRows = selectedImages.length < 3 ? 1 : 2;
+  const [existingImages, setExistingImages] = useState<string[]>([]);
   const [hasFreeTrial, setHasFreeTrial] = React.useState(false);
+  const navigation = useNavigation()
+  const numRows = selectedImages.length < 3 ? 1 : 2;
   const imagesKeys = []
   // Aller fetcher sur l'api pour savoir quelle sont les types de tarification de l'activité
   const [checkedItems, setCheckedItems] = useState<{ [key: string]: boolean; }>({
@@ -62,30 +63,6 @@ const NewActivityScreen = (clubId: string) => {
     onChange(checkedKeys); // Update the form control
   };
 
-  // const pickImageAsync = async () => {
-  //   try {
-  //   let result = await ImagePicker.launchImageLibraryAsync({
-  //     quality: 0.5,
-  //     allowsMultipleSelection: true,
-  //     mediaTypes: ImagePicker.MediaTypeOptions.Images,
-  //     // maxWidth: 450,
-  //     // maxHeight: 700,
-  //   });
-  //     if (!result.canceled) {
-  //       const images: string[] = []
-  //       result.assets.forEach(image => { images.push(image.uri) })
-  //       setSelectedImages(prevImages => [...prevImages, ...images]);
-  //       // console.log(selectedImages)
-
-  //     } else {
-  //       alert('You did not select any image.');
-  //     }
-  //   } catch (error) {
-  //     // Handle the error here
-  //     console.error('Error picking image:', error);
-  //     Alert.alert('Error', 'There was an error picking the image. Please try again.');
-  //   }
-  // };
   const pickImageAsync = async () => {
     try {
       setImagePickerVisible(true);
@@ -102,11 +79,10 @@ const NewActivityScreen = (clubId: string) => {
         setSelectedImages((prevImages) => [...prevImages, ...selectedImageUris]);
       }
     } catch (error) {
-      // Handle the error here
       console.error('Error picking image:', error);
       Alert.alert('Error', 'There was an error picking the image. Please try again.');
     } finally {
-      setImagePickerVisible(false); // Hide the activity indicator after the process completes (success or error)
+      setImagePickerVisible(false);
     }
   };
   const saveAndGoToActivity = (data: {}) => {
@@ -140,7 +116,6 @@ const NewActivityScreen = (clubId: string) => {
         console.log(activityObj.images, 'this is the object with images ready to be sent to the mutation')
         return activityObj
       }).then(async (activityObj) => {
-        // console.log(restaurantObj.images, 'this is restaurantObj.images ready to be sent to the mutation')
         try {
           console.log(activityObj, 'this is activityJob ready to be sent to the mutation')
           // await doCreateRestaurant({variables: {input: restaurantObj}});
@@ -168,7 +143,6 @@ const NewActivityScreen = (clubId: string) => {
 
   }
 
-  // Image uploading to S3 (then will have to update my db with the key returned)
   const uploadMedia = async(uri: string) => {
     // fetch the image from the local uri and upload it to s3 using the Storage.put method then return the key and set it to the imagesKeys state
     try {
@@ -190,6 +164,12 @@ const NewActivityScreen = (clubId: string) => {
     }
   }
 
+  const handleImageDelete = async (imageUri: string) => {
+    setSelectedImages((prevSelectedImages) =>
+      prevSelectedImages.filter((image) => image !== imageUri)
+    );
+  }
+
   return (
     <ScrollView style={{ padding: 15, flex: 1}}>
       {isSubmitting ? (
@@ -198,41 +178,29 @@ const NewActivityScreen = (clubId: string) => {
         <View style={{gap: 15}}>
           <Card>
             <Card.Title title="Ajoutez des photos de votre activité" />
-              {/* Component d'input pour des photos */}
-              {isImagePickerVisible ? (
-                <ActivityIndicator size="large" color="blue" style={{ marginTop: 20 }} />
+
+              { isImagePickerVisible ? (
+
+                <ActivityIndicator size="large" color="grey" style={{ marginTop: 20 }} />
+
               ) : selectedImages.length > 0 ? (
-                <View style={{borderWidth: 0, margin: 10}}>
-                  <FlatList
-                    data={selectedImages}
-                    keyExtractor={(item) => item}
-                    numColumns={3}
-                    scrollEnabled={false}
-                    style={{ maxHeight: numRows * 150 }}
-                    renderItem={({ item }) => (
-                      <View style={styles.thumbnailContainer}>
-                        <Image source={{ uri: item }} style={styles.thumbnail} />
-                        <Pressable style={styles.deleteButton}>
-                          <Ionicons
-                            onPress={ () => {
-                              const newImages = selectedImages.filter((image) => image !== item)
-                              setSelectedImages(newImages)
-                            }}
-                            name='trash-outline'
-                            size={20}
-                            color={colors.danger}
-                          />
-                        </Pressable>
-                      </View>
-                    )}
-                  />
-                  <Text onPress={pickImageAsync} style={{fontSize: 14, color: colors.grayDarkest, margin: 10}}>{selectedImages.length} images séléctionnées <Text style={{color: colors.danger}}>(Ajouter)</Text></Text>
-                </View>
+
+                <PhotosSection
+                  existingImages={existingImages}
+                  selectedImages={selectedImages}
+                  numRows={numRows} // Make sure to provide numRows as a prop
+                  pickImageAsync={pickImageAsync} // Make sure to provide pickImageAsync as a prop
+                  handleImageDelete={handleImageDelete}
+                />
+
               ) : (
+
                 <Button onPress={pickImageAsync} style={styles.imageButton}>
                   <Text style={styles.addImageText}>+</Text>
                 </Button>
-              )}
+
+              )
+              }
 
               {/* End of image component input */}
 
@@ -327,30 +295,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 0,
     marginTop: 1.5,
-  },
-  deleteButton: {
-    position: 'absolute',
-    bottom: -10,
-    right: 0,
-    backgroundColor: 'white',
-    borderRadius: 50,
-    shadowOffset: { width: 2, height: 0 }, // iOS only
-    shadowOpacity: 0.8, // iOS only
-    elevation: 8, // Android only
-  },
-  thumbnailContainer: {
-    margin: 4,
-    marginBottom: 10,
-    width: "30%",
-    aspectRatio: 1,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'black'
-    },
-  thumbnail: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 10
   },
   imageButton: {
     width: "95%",
