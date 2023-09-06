d4d29ca5951b21fa8d2596efb8be18735b989df3
Etienne de Dianous <etiennededi@hotmail.fr>
2023-09-06 17:59:34 +0200

    ok

--- amplify/backend/function/YouclubPostConfirmation/src/custom.js
+++ amplify/backend/function/YouclubPostConfirmation/src/custom.js
@@ -5,17 +5,15 @@ exports.handler = async (event, context) => {
   // insert code to be executed by your lambda trigger
   const axios = require('axios');
   const userEmail = event.request.userAttributes.email;
+  const userSubId = event.request.userAttributes.sub;
   const userPassword = event.request.userAttributes.password;
 
-  console.log("HEEEEEY LAMBDA FUNCTION")
-  console.log(event)
   console.log(userEmail, '<-- USER EMAIL')
-  console.log(userPassword, '<-- USER PASSWORD')
   // 1. Check if user exist
   try {
     const response = await axios.post('https://youclubstaging-42da65c4b5e7.herokuapp.com/graphql', {
       query: `
-        userByEmail(email: ${userEmail}) {
+        userByEmail(email: "${userEmail}") {
           id
           email
           role
@@ -23,20 +21,23 @@ exports.handler = async (event, context) => {
       `,
     });
     console.log(response, '<-- RESPOOOOONSE')
-    console.log(response?.data?.data?.userByEmail, 'this is data userByEmail')
+    console.log(response?.data, '<-- RESPOOOOONSE DATA')
+    console.log(response?.data?.data, '<-- RESPOOOOONSE DATA.DATA')
+    console.log(response?.data?.data?.userByEmail, '<----- response?.data?.data?.userByEmail')
 
     // If user exist, return the user
     if (response?.data?.data?.userByEmail) {
-      return response?.data?.data?.userByEmail;
+      return response.data.data.userByEmail;
     }
     // If user doesn't exist, create a new user
     const newUserResponse = await axios.post('https://youclubstaging-42da65c4b5e7.herokuapp.com/graphql', {
       query: `
         createUser(input: {
-          email: ${userEmail},
-          password: ${userPassword},
-          password_confirmation: ${userPassword},
-          role: 'user'
+          email: "${userEmail}",
+          password: "12345678",
+          password_confirmation: "12345678",
+          role: "user",
+          sub_id: "${userSubId}"
         }) {
           id
           email
@@ -45,6 +46,7 @@ exports.handler = async (event, context) => {
         }
       `,
     });
+    console.log(newUserResponse, '<-- NEW USER RESPONSE')
     return newUserResponse?.data?.data?.createUser;
   } catch (error) {
     console.error(error);
